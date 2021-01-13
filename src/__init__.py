6eea1cd3da793e43e231a68fe29349dd56dbee2e
abhig8 <abhinavgcloud@gmail.com>
2021-01-12 20:10:48 -0800

    Initial Commit

---
+++ src/__init__.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, session, request, url_for, redirect
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, asc, Float
+from datetime import datetime
+from stock_info import ticker_investment
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+app = Flask(__name__)
+
+db_name = "stock.db"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# need to figure out how to update check_number here upon drop down click and displaying different dates on pages
+check_number = 0
+
+class Stock(db.Model):
+    id = db.Column(db.Integer , primary_key=True , autoincrement=True)
+    ticker = db.Column(db.String())
+    stock = db.Column(db.String())
+    price = db.Column(db.Float)
+    updated = db.Column(db.String())
+    first_check = db.Column(db.String())
+
+def get_standard_time(date_time):
+	date = datetime.strptime(date_time[:10], '%Y-%m-%d').strftime('%m-%d-%Y')
+	time = date_time[11:]
+	hour = int(time[:2])
+	is_am = hour < 12
+	if not is_am:
+		hour -=12
+	if not hour:
+		hour = 12
+	time = str(hour) + time[2:]
+	if is_am:
+		 time += " AM "
+	else:
+		time += " PM "
+	time += "EST"
+	return date + " @ " + time
+
+def top_stocks(number):
+	recent_stocks = Stock.query.order_by(*[Stock.id.desc(), Stock.price.asc()]).limit(len(ticker_investment)).all()
+	# recent_stocks.sort(key=lambda x: float(x.first_check), reverse=True)
+	card_values = []
+	for x in range(number):
+		if not check_number:
+			price = float(recent_stocks[x].first_check)
+		else:
+			price = float(recent_stocks[x].second_check)
+		card_values.append([recent_stocks[x].stock, recent_stocks[x].ticker, "{:,.2f}".format(price), int((price-1200)/12)])
+	return card_values
+
+@app.route("/")
+def home():
+	top = top_stocks(8)
+	return render_template("home.html", top_4 = top[:4], last_4 = top[4:])
+
+@app.route("/<stock_ticker>")
+def stock(stock_ticker):
+	try:
+		stock_data = Stock.query.filter_by(ticker=stock_ticker).order_by(Stock.id.desc()).first()
+		if not check_number:
+			price = float(stock_data.first_check)
+		else:
+			price = float(stock_data.second_check)
+		return render_template("stock.html", stock_name=stock_data.stock, stock_ticker=stock_ticker, 
+		stock_price = "{:,.2f}".format(price), last_updated = get_standard_time(stock_data.updated),
+		percentage = int((price-1200)/12))
+	except Exception as e:
+		print(e)
+		return redirect(url_for("home"))
+
+
+@app.route("/search")
+def search():
+	return stock(request.args.get("q"))
+
+
+@app.route("/overview")
+def overview():
+	return render_template("overview.html", investment_list=top_stocks(len(ticker_investment)))
+
+
+if __name__ == "__main__":
+	app.run(debug=False)
+	# app.run()
+
+
+
+
---
+++ src/stock_info.py
@@ -0,0 +1,41 @@
+
+
+tickers = ["TSLA", "GOOGL", "AAPL", "PTON", "FB", "AMZN", "NFLX", "BTC", "ETH", "DOGE", "XRP", "LTC"]
+
+ticker_stock = {"TSLA": "TESLA", "GOOGL": "GOOGLE", "AAPL": "APPLE", "PTON": "PELOTON",
+ "FB": "FACEBOOK", "AMZN": "AMAZON", "NFLX": "NETFLIX"}
+ticker_crypto = {"BTC": "BITCOIN", "ETH": "ETHEREUM", "DOGE": "DOGECOIN", "XRP": "RIPPLE", "LTC": "LITECOIN"}
+
+ticker_investment = {**ticker_stock, **ticker_crypto}
+
+tickers_second = list(ticker_stock.keys()) + list(ticker_crypto.keys())
+
+prices_april = [130.19, 1210.41, 68.31, 31.99, 174.79, 2168.87, 396.72, 6861.21, 156.62, .0020, .1877, 41.20]
+
+prices_december = []
+
+"""
+adjusted for stock split:
+
+* crypto can't be split *
+
+- TSLA: August 31, 2020 (5 for 1)
+	WAS 650.95, ADJUSTED FOR 1/5 - NOW 130.19
+- AAPL: August 28, 2020 (4 for 1)
+	WAS 273.24, ADJUSTED FOR 1/4 - NOW 68.31
+"""
+
+ticker_price_april = { }
+
+for index in range(len(tickers)):
+	ticker_price_april[tickers[index]] = prices_april[index]
+
+
+ticker_price_april_second = {'TSLA': 130.19, 'GOOGL': 1210.41, 'AAPL': 68.31, 'PTON': 31.99, 'FB': 174.79, 'AMZN': 2168.87, 'NFLX': 396.72, 'BTC': 6861.21, 'ETH': 156.62, 'DOGE': 0.002, 'XRP': 0.1877, 'LTC': 41.2}
+
+ticker_price_december = { }
+
+# for index in range(len(tickers)):
+# 	ticker_price_december[tickers[index]] = prices_december[index]
+
+
