1365152cb1237fcb471431480162bae6ee59e7fa
abhig8 <abhinavgcloud@gmail.com>
2021-06-06 15:44:19 -0700

    adding investments to dropdown list

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder = "templates", static_folder = "static")
 # db_name = "stock.db"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 
-#DATABASE_URL = "postgresql://aaclbzejzdxebt:eba4ca8018075b68e2c553d37745eb9b16194d663c1fd15ba85c7e3c934fae64@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d119nni8ln3u0i"
+# DATABASE_URL = "postgresql://aaclbzejzdxebt:eba4ca8018075b68e2c553d37745eb9b16194d663c1fd15ba85c7e3c934fae64@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d119nni8ln3u0i"
 DATABASE_URL = os.environ['DATABASE_URL']
 
 
