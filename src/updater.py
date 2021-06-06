298eb624ca8afff4d7ffb49c16ae389f004609d3
abhig8 <abhinavgcloud@gmail.com>
2021-06-06 15:51:05 -0700

    adding att query

--- src/updater.py
+++ src/updater.py
@@ -15,8 +15,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 
-# DATABASE_URL = "postgresql://aaclbzejzdxebt:eba4ca8018075b68e2c553d37745eb9b16194d663c1fd15ba85c7e3c934fae64@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d119nni8ln3u0i"
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = "postgresql://aaclbzejzdxebt:eba4ca8018075b68e2c553d37745eb9b16194d663c1fd15ba85c7e3c934fae64@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d119nni8ln3u0i"
+
 
 # import matplotlib.pyplot as plt
 
@@ -105,17 +106,25 @@ def update_cryptos():
 		c.execute('insert into stock (ticker, stock, price, updated, first_check) values (%s,%s,%s,%s,%s)', investment)
 	conn.commit()
 
-def update_investment(ticker):
-	url = requests.get('https://finance.yahoo.com/quote/' + ticker + '-USD?p=' + ticker + '-USD')
-	investment = ticker_investment.get(ticker)
-	soup = bs4.BeautifulSoup(url.text, features="html.parser")
-	price = float(soup.find_all("div", {'class': 'D(ib) smartphone_Mb(10px) W(70%) W(100%)--mobp smartphone_Mt(6px)'})[0].find('span').text.replace(',',''))
+def update_investment(ticker, investment_type):
+	if investment_type == "stock":
+		url = requests.get('https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker)
+		investment = ticker_investment.get(ticker)
+		soup = bs4.BeautifulSoup(url.text, features="html.parser")
+		price = float(soup.find_all("div", {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text.replace(',',''))
+	else:
+		url = requests.get('https://finance.yahoo.com/quote/' + ticker + '-USD?p=' + ticker + '-USD')
+		investment = ticker_investment.get(ticker)
+		soup = bs4.BeautifulSoup(url.text, features="html.parser")
+		price = float(soup.find_all("div", {'class': 'D(ib) smartphone_Mb(10px) W(70%) W(100%)--mobp smartphone_Mt(6px)'})[0].find('span').text.replace(',',''))
 	date_time = datetime.datetime.now().strftime('%m-%d-%Y %I:%M:%S %p')
 	first_check =  "{0:.2f}".format(1200/ticker_price_april.get(ticker)*price)
 	c.execute('insert into stock (ticker, stock, price, updated, first_check) values (%s,%s,%s,%s,%s)', [ticker, investment, price, date_time, first_check])
 
 # update_cryptos()
 # update_stocks()
+
+#update_investment("T", "stock")
 
 
 
